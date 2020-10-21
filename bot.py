591f8cc252778db8fdc2013eb6657577d6fd37f4
wideputn <73242010+wideputn@users.noreply.github.com>
2020-10-22 00:17:26 +0530

    Addition: Clear command

--- bot.py
+++ bot.py
@@ -11,4 +11,10 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send("Hello :wave: , I am Nitro. How's it going? ")
 
+@client.command(aliases=['c'])
+@commands.has_permissions(manage_messages = True)
+async def clear(ctx, amount=2):
+    await ctx.channel.purge(limit=amount)
+
+
 client.run(os.environ["token"])
